fd2ad834e29db8d74665585c3cf46dcd5f3f1a72
fridaSea <maria.kluge.codes@gmail.com>
2026-05-15 14:17:06 +0200

    Add variant (navbar) and activity status check for button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,17 +3,19 @@ import "./Button.css";
 interface MainButtonProps {
   text: string;
   onClick: () => void;
-  variant?: "primary" | "secondary" | "active";
+  variant?: "primary" | "secondary" | "active" | "navbar";
+  active?: boolean;
 }
 
-function MainButton({ text, onClick, variant }: MainButtonProps) {
+function MainButton({ text, onClick, variant, active }: MainButtonProps) {
   return (
     <div>
       <button
-        className={`main-button ${variant ? `main-button--${variant}` : ""}`}
+        className={`main-button ${variant ? `main-button--${variant}` : ""} ${active ? "active" : ""}`}
         onClick={onClick}
       >
         {text}
+        {/* {text} {active ? "YES" : "NO"} */}
       </button>
     </div>
   );
