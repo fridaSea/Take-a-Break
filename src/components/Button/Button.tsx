a1341873d86131e49dadf286c9c793bd810cb984
fridaSea <maria.kluge.codes@gmail.com>
2026-06-01 12:53:52 +0200

    Adapt css, so all the css is wrapped in the container & Add variant "close"

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,13 +4,13 @@ interface MainButtonProps {
   text: string;
   // onClick: () => void;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
-  variant?: "primary" | "secondary" | "active" | "navbar";
+  variant?: "primary" | "secondary" | "active" | "navbar" | "close";
   active?: boolean;
 }
 
 function MainButton({ text, onClick, variant, active }: MainButtonProps) {
   return (
-    <div>
+    <div className="button-container">
       <button
         className={`main-button ${variant ? `main-button--${variant}` : ""} ${active ? "active" : ""}`}
         onClick={onClick}
