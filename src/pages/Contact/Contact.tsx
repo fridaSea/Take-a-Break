1b2aed623a2714f2770390ff54f95a6baccf4f3c
fridaSea <maria.kluge.codes@gmail.com>
2026-07-05 11:55:45 +0200

    Add contact page

---
+++ src/pages/Contact/Contact.tsx
@@ -0,0 +1,14 @@
+import "./Contact.css";
+
+function Contact() {
+  // TODO: Change FutureSelf to Personality
+  return (
+    <>
+      <div className="contact-container">
+        <p>Hier könnte ihre Kontakt E-Mail stehen</p>
+      </div>
+    </>
+  );
+}
+
+export default Contact;
