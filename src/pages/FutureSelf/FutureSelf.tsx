71829e37d846c0bda6204caa7bf9977336b6025b
fridaSea <maria.kluge.codes@gmail.com>
2026-07-05 12:37:19 +0200

    Add type

--- src/pages/FutureSelf/FutureSelf.tsx
+++ src/pages/FutureSelf/FutureSelf.tsx
@@ -81,7 +81,7 @@ function FutureSelf() {
     setSelectedPersonality(personality);
   };
 
-  const getChosenPersonality = () => {
+  const getChosenPersonality = (): Personality | null => {
     const chosenPersonalityId = localStorage.getItem("chosenPersonality");
 
     if (chosenPersonalityId) {
