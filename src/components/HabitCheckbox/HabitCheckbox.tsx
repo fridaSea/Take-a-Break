ce060c22827c2cc42980186dcf3f5a86a41ce7a3
fridaSea <maria.kluge.codes@gmail.com>
2026-07-05 12:21:17 +0200

    Disable code that isn’t ready for deployment

--- src/components/HabitCheckbox/HabitCheckbox.tsx
+++ src/components/HabitCheckbox/HabitCheckbox.tsx
@@ -1,55 +1,55 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import "./HabitCheckbox.css";
 
 interface HabitCheckboxProps {
   habitName: string;
 }
 function HabitCheckbox({ habitName }: HabitCheckboxProps) {
-  const [checkbox, setCheckbox] = useState<boolean>(false);
+  // const [checkbox, setCheckbox] = useState<boolean>(false);
 
-  const [habitArray, setHabitArray] = useState(() => {
-    const savedHabit = localStorage.getItem(habitName);
-    return savedHabit ? JSON.parse(savedHabit) : [];
-  });
+  // const [habitArray, setHabitArray] = useState(() => {
+  //   const savedHabit = localStorage.getItem(habitName);
+  //   return savedHabit ? JSON.parse(savedHabit) : [];
+  // });
 
-  //   console.log("habitArray", habitArray);
+  // //   console.log("habitArray", habitArray);
 
-  useEffect(() => {
-    localStorage.setItem("habitName", JSON.stringify(habitArray));
-  }, [habitArray]);
+  // useEffect(() => {
+  //   localStorage.setItem("habitName", JSON.stringify(habitArray));
+  // }, [habitArray]);
 
-  function handleCheckboxClick() {
-    const newValue = !checkbox; // "Nimm den aktuellen Status der Checkbox (checkbox) und kehre ihn um (!). Speichere das Ergebnis in newValue."
-    setCheckbox(newValue);
-    localStorage.setItem(habitName, newValue.toString());
-  }
+  // function handleCheckboxClick() {
+  //   const newValue = !checkbox; // "Nimm den aktuellen Status der Checkbox (checkbox) und kehre ihn um (!). Speichere das Ergebnis in newValue."
+  //   setCheckbox(newValue);
+  //   localStorage.setItem(habitName, newValue.toString());
+  // }
 
-  function loadFromLocalStorage() {
-    const savedHabit = localStorage.getItem(habitName);
-    if (savedHabit !== null) {
-      //ungleich zu null
-      if (savedHabit === "true") {
-        setCheckbox(true);
-      } else {
-        setCheckbox(false);
-      }
-    }
-  }
+  // function loadFromLocalStorage() {
+  //   const savedHabit = localStorage.getItem(habitName);
+  //   if (savedHabit !== null) {
+  //     //ungleich zu null
+  //     if (savedHabit === "true") {
+  //       setCheckbox(true);
+  //     } else {
+  //       setCheckbox(false);
+  //     }
+  //   }
+  // }
 
-  // const addHabit = (newHabit) => {
-  //   setHabitArray((prevArray) => [...prevArray, newHabit]);
-  // };
+  // // const addHabit = (newHabit) => {
+  // //   setHabitArray((prevArray) => [...prevArray, newHabit]);
+  // // };
 
-  useEffect(() => {
-    loadFromLocalStorage();
-  }, []);
+  // useEffect(() => {
+  //   loadFromLocalStorage();
+  // }, []);
 
   return (
     // NEXT NEUE GEWOHNHEIT HINZUFÜGEN
     // NEXT ICON FÜR GEWOHNHEIT AUSWÄHLEN
     // NEXT VERLSUF ANZEIGEN
     <div className="habit-container">
-      <div className="habit-item">
+      {/* <div className="habit-item">
         <span className="habit-name">{habitName}</span>
         <input
           type="checkbox"
@@ -57,7 +57,7 @@ function HabitCheckbox({ habitName }: HabitCheckboxProps) {
           className="habit-checkbox"
           onChange={handleCheckboxClick}
         />
-      </div>
+      </div> */}
     </div>
   );
 }
